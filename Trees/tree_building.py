9b5c916155aa0c5cb1d14389a2ae390a23e6af66
Amit Pratap <00amitpratap@gmail.com>
2025-02-17 19:57:51 +0530

    Pre-inoder tree building

--- Trees/tree_building.py
+++ Trees/tree_building.py
@@ -39,6 +39,30 @@ def levelorder(root):
         if currNode.right is not None:
             nodes.put(currNode.right)
         print(currNode.data, end=" ")
+
+def search(inorder_, val, left, right):
+    for i in range(left, right+1):
+        if inorder_[i] == val:
+            return i
+    return -1
+
+def build_tree(inorder_, preorder_,idx, left, right):
+    if left > right or idx >= len(preorder_):
+        return None
+    # print(idx, left, right)
+    
+    val = preorder_[idx]
+    idx += 1
+
+    root_tmp = Node(val)
+    idx_in = search(inorder_, val, left, right)
+
+    root_tmp.left = build_tree(inorder_, preorder_, idx, left, idx_in-1)
+    root_tmp.right = build_tree(inorder_, preorder_, idx, idx_in +1, right)
+
+    return root_tmp
+
+    
     
 root = Node(1)
 root.left = Node(2)
@@ -48,7 +72,7 @@ root.left.left = Node(4)
 root.left.right = Node(5)
 
 root.right.right = Node(6)
-root.right.right.left = Node(7)
+# root.right.right.left = Node(7)
 print('Preorder: ',end=" ")
 preorder(root)
 print()
@@ -57,10 +81,15 @@ print('Inorder: ',end=" ")
 inorder(root)
 print()
 
-print('Postorder: ',end=" ")
-postorder(root)
+preorder_ = [1,2,4,5,3,6]
+inorder_ = [4,2,5,1,3,6]
+print(f"len: {len(preorder_)}")
+root_tmp = build_tree(inorder_, preorder_, 0,0,len(preorder_)-1)
+
+print('Preorder: ',end=" ")
+preorder(root)
 print()
 
-print('Level order: ',end=" ")
-levelorder(root)
+print('Inorder: ',end=" ")
+inorder(root)
 print()
